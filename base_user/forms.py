d07091bf4e45419fda4195ae44c5c93a89177814
safaraliyevelmir <safaraliyevelmir@gmail.com>
2022-06-06 09:14:26 +0400

    first

---
+++ base_user/forms.py
@@ -0,0 +1,37 @@
+from .models import CustomUser
+from django import forms
+
+class UserRegisterForm(forms.ModelForm):
+    
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Re-Password'}))
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            'email',
+        )
+    
+        widgets = {
+            'email':forms.TextInput(attrs={'placeholder':'E-Mail'}),        
+        }
+    
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages["Password isn't match"],
+                code="password_isn't match",
+            )
+        return password2
+
+    
+
+    def save(self, commit=True):
+        user = super(UserRegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
---
+++ base_user/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.core.mail import send_mail
+from uuid import uuid4
+from django.contrib import messages
+from django.contrib.auth import get_user_model, login,authenticate
+from django.conf import settings
+User = get_user_model()
+
+def loginUser(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user_obj = User.objects.filter(email=email).first()
+        if user_obj is None:
+            messages.error(request,"User not found")
+            return redirect('login')
+        user = authenticate(email=email,password=password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+        else:
+            messages.error(request,"Password is incorrect")
+            return redirect('login')
+    return render(request,'login.html')
+
+def register(request):   
+    form = UserRegisterForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user =form.save(commit=False)
+            user.token = uuid4()
+            user.is_active = False
+            email=user.email
+            form.save()
+            send_confirmation_email(request,email)
+            return redirect('login')
+    context = {
+        'form':form
+    }   
+    return render(request,'register.html',context)
+
+
+def verify(request,token):
+    user = User.objects.filter(token=token).first()
+    user.is_active = True
+    user.save()
+    context = {
+        'email':user.email,
+    }
+    return render(request,'verify.html',context)
+
+def send_confirmation_email(request,email):
+    user = User.objects.filter(email=email).first()
+    host = request.META['HTTP_HOST']
+    subject = "Confirmation Email by Qsmart"
+    message = f"Please click this link and verify your account {host}/user/verify/{user.token}/"
+    from_email = settings.EMAIL_HOST_USER
+    recipient_list = [email]
+    send_mail(subject, message, from_email, recipient_list)
+
+    
