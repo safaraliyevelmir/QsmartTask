f217f6a7060af8b56e58e815f18f61a83770ac5b
safaraliyevelmir <safaraliyevelmir@gmail.com>
2022-06-06 09:21:59 +0400

    ready for heroku

--- core/settings.py
+++ core/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import django_heroku
 
 from pathlib import Path
 
@@ -140,3 +141,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+django_heroku.settings(locals())
