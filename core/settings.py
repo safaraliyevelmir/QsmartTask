25e5fe11b57ea5267857c0d22ce37df00683b718
safaraliyevelmir <safaraliyevelmir@gmail.com>
2022-06-06 09:32:57 +0400

    login required

--- core/settings.py
+++ core/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
     'static'
 ]
 LOGIN_URL = '/user/login/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
